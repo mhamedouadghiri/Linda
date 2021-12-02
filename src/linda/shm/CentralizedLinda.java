e9ee3ac9d48707055668e5ec184dcd7de5ad8b04
M'hamed SEFIANE-EL OUADGHIRI <56483502+mhamedouadghiri@users.noreply.github.com>
2021-12-02 23:02:56 +0100

    feat: implement some simple CentralizedLinda methods

--- src/linda/shm/CentralizedLinda.java
+++ src/linda/shm/CentralizedLinda.java
@@ -4,17 +4,32 @@ import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-/** Shared memory implementation of Linda. */
+/**
+ * Shared memory implementation of Linda.
+ */
 public class CentralizedLinda implements Linda {
+
+    private final List<Tuple> tuples = new ArrayList<>();
+    private final Lock lock = new ReentrantLock();
 
     public CentralizedLinda() {
     }
 
     @Override
     public void write(Tuple t) {
-
+        if (t == null) {
+            System.err.println("LOG (w): Attempted to persist a null tuple, operation aborted.");
+            return;
+        }
+        lock.lock();
+        tuples.add(t.deepclone());
+        lock.unlock();
     }
 
     @Override
@@ -29,22 +44,75 @@ public class CentralizedLinda implements Linda {
 
     @Override
     public Tuple tryTake(Tuple template) {
-        return null;
+        if (template == null) {
+            return null;
+        }
+        lock.lock();
+        Tuple hit = null;
+        for (Tuple tuple : tuples) {
+            if (tuple.matches(template)) {
+                hit = tuple.deepclone();
+                tuples.remove(hit);
+                break;
+            }
+        }
+        lock.unlock();
+        return hit;
     }
 
     @Override
     public Tuple tryRead(Tuple template) {
-        return null;
+        if (template == null) {
+            return null;
+        }
+        lock.lock();
+        Tuple hit = null;
+        for (Tuple tuple : tuples) {
+            if (tuple.matches(template)) {
+                hit = tuple.deepclone();
+                // the spec does not specify which one to get, so we return the first one encountered
+                break;
+            }
+        }
+        lock.unlock();
+        return hit;
     }
 
     @Override
     public Collection<Tuple> takeAll(Tuple template) {
-        return null;
+        if (template == null) {
+            return new ArrayList<>();
+        }
+        lock.lock();
+        List<Tuple> hits = new ArrayList<>();
+        // store all the right tuples
+        for (Tuple tuple : tuples) {
+            if (tuple.matches(template)) {
+                hits.add(tuple);
+            }
+        }
+        // then remove them from the shared memory
+        for (Tuple hit : hits) {
+            tuples.remove(hit);
+        }
+        lock.unlock();
+        return hits;
     }
 
     @Override
     public Collection<Tuple> readAll(Tuple template) {
-        return null;
+        if (template == null) {
+            return new ArrayList<>();
+        }
+        lock.lock();
+        List<Tuple> hits = new ArrayList<>();
+        for (Tuple tuple : tuples) {
+            if (tuple.matches(template)) {
+                hits.add(tuple);
+            }
+        }
+        lock.unlock();
+        return hits;
     }
 
     @Override
@@ -54,7 +122,7 @@ public class CentralizedLinda implements Linda {
 
     @Override
     public void debug(String prefix) {
-
+        tuples.forEach(t -> System.out.println(prefix + t));
     }
 
     // TO BE COMPLETED
