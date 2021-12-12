e9053323323b821abe1834df4a326d86381c0926
M'hamed SEFIANE-EL OUADGHIRI <56483502+mhamedouadghiri@users.noreply.github.com>
2021-12-12 20:02:43 +0100

    test: add immediate read re-registering callback loop test

---
+++ src/linda/test/TestCallbackLoop.java
@@ -0,0 +1,34 @@
+package linda.test;
+
+import linda.Callback;
+import linda.Linda;
+import linda.Linda.eventMode;
+import linda.Linda.eventTiming;
+import linda.Tuple;
+
+/***
+ * Will enter an infinite loop, raise a StackOverflowError exception then crash!
+ */
+public class TestCallbackLoop {
+
+    private static Linda linda;
+    private static Tuple temp;
+
+    public static void main(String[] a) {
+        linda = new linda.shm.CentralizedLinda();
+
+        temp = new Tuple(Integer.class, String.class);
+
+        Tuple t = new Tuple(42, "foobar");
+        linda.write(t);
+
+        linda.eventRegister(eventMode.READ, eventTiming.IMMEDIATE, temp, new MyCallback());
+    }
+
+    private static class MyCallback implements Callback {
+        public void call(Tuple t) {
+            System.out.println("CB got " + t);
+            linda.eventRegister(eventMode.READ, eventTiming.IMMEDIATE, temp, this);
+        }
+    }
+}
