03836cfdc6f78c55534f88499b0d887522a7f633
M'hamed SEFIANE-EL OUADGHIRI <56483502+mhamedouadghiri@users.noreply.github.com>
2022-01-15 19:54:29 +0100

    apps: add eratosthenes sieve app

---
+++ src/linda/applications/EratosthenesSieve.java
@@ -0,0 +1,87 @@
+package linda.applications;
+
+import linda.Linda;
+import linda.Tuple;
+import linda.shm.CentralizedLinda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EratosthenesSieve {
+    public static void main(String[] args) {
+        int n = 10_000;
+        int iterations = 10;
+        System.out.printf("Launching %d iteration of Eratosthenes for primes up to %d\n", iterations, n);
+        benchmark(n, iterations);
+    }
+
+    private static List<Integer> normalSieve(int n) {
+        boolean[] booleans = new boolean[n];
+        Arrays.fill(booleans, true);
+
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (booleans[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    booleans[j] = false;
+                }
+            }
+        }
+
+        return IntStream.range(2, booleans.length).filter(i -> booleans[i]).boxed().collect(Collectors.toList());
+    }
+
+    private static List<Integer> lindaSieve(int n) {
+        Linda linda = new CentralizedLinda();
+
+        List<Integer> primes = new ArrayList<>();
+
+        IntStream.rangeClosed(2, n).mapToObj(i -> new Tuple(i)).forEach(linda::write);
+
+        for (int i = 2; i < n + 1; i++) {
+            Tuple tuple = linda.tryRead(new Tuple(i));
+            if (tuple != null) {
+                int prime = (Integer) tuple.element();
+                primes.add(prime);
+                for (int j = prime * prime; j < n + 1; j += prime) {
+                    linda.tryTake(new Tuple(j));
+                }
+            }
+        }
+
+        return primes;
+    }
+
+    private static void benchmark(int n, int iterations) {
+        double time;
+        int size = -1;
+
+        List<Long> longs = new ArrayList<>();
+        for (int i = 0; i < iterations; i++) {
+            long start = System.currentTimeMillis();
+            size = normalSieve(n).size();
+            long end = System.currentTimeMillis();
+            longs.add(end - start);
+        }
+        time = longs.stream().mapToLong(value -> value).average().orElse(Double.NaN) / 1_000;
+        printSummary("Normal Sieve", size, time);
+
+        longs = new ArrayList<>();
+        for (int i = 0; i < iterations; i++) {
+            long start = System.currentTimeMillis();
+            size = lindaSieve(n).size();
+            long end = System.currentTimeMillis();
+            longs.add(end - start);
+        }
+        time = longs.stream().mapToLong(value -> value).average().orElse(Double.NaN) / 1_000;
+        printSummary("Linda Sieve", size, time);
+    }
+
+    private static void printSummary(String type, int size, double time) {
+        System.out.println(type + ": ");
+        System.out.println("\tnb of primes: " + size);
+        System.out.printf("\ttime: %.5f sec\n", time);
+    }
+}
